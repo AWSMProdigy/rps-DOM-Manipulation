350758b486e2f91951358a3b5493d7eabb8a8404
Dan Rosenbaum <drosenbaum5@gmail.com>
2021-03-25 16:47:59 -0400

    Reconfigured clearAlerts() function into clearPreviousAlerts(). Attached function call to beginning of compareChoices() to successfully get alerts to display

--- script.js
+++ script.js
@@ -10,37 +10,29 @@ const rps = ["rock", "paper", "scissors"];
 
 // Start the game
 function startGame() {
-   
-
-  
-  attachButtons()
-  // Call score summary function here
+  console.log(rounds);
+  let btns = document.querySelectorAll(".btn");
+  console.log(btns);
+  btns.forEach(function (btn) {
+    btn.addEventListener("click", function () {
+      let userChoice = event.target.id; // Could also be this.id
+      // console.log("user " + userChoice);
+      let compChoice = computerChoice();
+      // console.log("computer " + compChoice);
+      compareChoices(userChoice, compChoice);
+    });
+  });
 }
 
 // TODO Figure out how to make alerts appear/dissapear properly
 
-function attachButtons () {
-  if (rounds <= 5) {
-    rounds++;
-    console.log(rounds);
-    let btns = document.querySelectorAll(".btn");
-    console.log(btns);
-    btns.forEach(function (btn) {
-      btn.addEventListener("click", function () {
-        let userChoice = event.target.id; // Could also be this.id
-        // console.log("user " + userChoice);
-        let compChoice = computerChoice();
-        // console.log("computer " + compChoice);
-        compareChoices(userChoice, compChoice);
-      });
-    });
-  }
-}
 
+  
 
 
 // Computer choice function
 function computerChoice() {
+  
   randomIndex = Math.floor(Math.random() * rps.length);
   // console.log(randomIndex);
   let computerChoice = rps[randomIndex];
@@ -50,6 +42,7 @@ function computerChoice() {
 
 // Function for comparing choices and deciding a winner
 function compareChoices(userSelection, computerSelection) {
+  clearPreviousAlerts()
   if (userSelection === computerSelection) {
     ties++;
   } else if (
@@ -59,40 +52,33 @@ function compareChoices(userSelection, computerSelection) {
   ) {
     playerWins++;
     computerLosses++;
-    // document.getElementById("player-success").style.display = "block";
-    // document.getElementById("computer-failure").style.display = "block";
-    $('#player-success').show()
-    $('#computer-failure').show()
-  
+    $("#player-success").show();
+    $("#computer-failure").show();
   } else {
     playerLosses++;
     computerWins++;
-    document.getElementById("computer-success").style.display = "block";
-    document.getElementById("player-failure").style.display = "block";
-    $('#player-failure').show()
-    $('#computer-success').show()
-  
+    $("#player-failure").show();
+    $("#computer-success").show();
   }
 
-  clearAlerts();
-  console.log("user: " + userSelection);
-  console.log("computer: " + computerSelection);
+  // console.log("user: " + userSelection);
+  // console.log("computer: " + computerSelection);
   console.log("Wins: " + playerWins);
   console.log("Losses: " + playerLosses);
   console.log("Ties: " + ties);
   printResults();
 }
 
-function clearAlerts() {
+function clearPreviousAlerts() {
   console.log("function firing");
-  $('#player-success').hide()
-  $('#computer-failure').hide()
-  $('#player-success').hide()
-  $('#computer-failure').hide()
+  $("#player-success").hide();
+  $("#computer-success").hide();
+  $("#player-failure").hide();
+  $("#computer-failure").hide();
 }
 
-function printResults() {}
+function printResults() {
+  console.log("Results placeholder");
+}
 
 startGame();
-
-
